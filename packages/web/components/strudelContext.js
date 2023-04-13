90abe48e7c8dc01e535aab6cf806fd309d221741
alex <alex@slab.org>
2023-04-13 09:23:50 +0100

    incoming osc

--- packages/web/components/strudelContext.js
+++ packages/web/components/strudelContext.js
@@ -1,4 +1,5 @@
 import("@strudel.cycles/core");
+import OSC from 'osc-js';
 
 var parts = ["waist", "back", "head"];
 
@@ -111,4 +112,38 @@ window.patternMove = (pat) => inhabit(moves, pat);
 
 for (var key in window.moves) {
   window[key] = window.moves[key];
+}
+
+
+if (!('oscinit' in window)) {
+  const osc = new OSC()
+  osc.on('/cps', (message, rinfo) => {
+    setcps(message.args[0])
+  });
+  osc.on('/ctrl', (message, rinfo) => {
+    const args = message.args;
+    var name = args.shift();
+    var listname = '_' + name;
+    if (!(listname in window)) {
+      window[listname] = args;
+      window[name] = pure(window[listname]).fmap(fastcat).innerJoin()
+    }
+    else {
+      window[listname].splice(0, window[listname].length, ...args);
+    }
+  });
+
+  osc.on('/move', (message, rinfo) => {
+    const args = message.args;
+    const name = args.shift();
+    const value = args.shift();
+    const dur = args.shift();
+    
+    if ('draw_incoming' in window) {
+      window.draw_incoming['name'].push({value: value, dur: dur});
+    }
+  });
+
+  osc.open()
+  window.oscinit = 1;
 }
